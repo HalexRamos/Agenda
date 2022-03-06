ff790128cb0c867b55f60af6b1805853c2c0848c
halexramos <halexramos96@gmail.com>
2022-03-06 13:32:40 -0300

    listando conteudo do formulario

--- app/src/main/java/alura/com/br/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/alura/com/br/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,9 +2,16 @@ package alura.com.br.agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import alura.com.br.agenda.DAO.AlunoDAO;
 import alura.com.br.agenda.R;
+import alura.com.br.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +19,22 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        AlunoDAO dao = new AlunoDAO();
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(view ->  {
+            String nome = campoNome.getText().toString();
+            String telefone = campoTelefone.getText().toString();
+            String email = campoEmail.getText().toString();
+
+            Aluno alunoCriado = new Aluno(nome, telefone, email);
+            dao.salva(alunoCriado);
+
+            startActivity(new Intent(FormularioAlunoActivity.this, ListaAlunosActivity.class));
+        });
     }
 }
--- app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import alura.com.br.agenda.DAO.AlunoDAO;
 import alura.com.br.agenda.R;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -18,9 +19,12 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        AlunoDAO dao = new AlunoDAO();
+
         setTitle("Lista de alunos");
-        List<String> alunos = new ArrayList<>(Arrays.asList("Marcos", "Lucas", "José", "Paulo", "Maria", "André"));
+
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
     }
 }
