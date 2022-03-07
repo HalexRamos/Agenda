a12320eca9561bf7c6949e6cef3adf0102dbefa7
halexramos <halexramos96@gmail.com>
2022-03-07 00:08:27 -0300

    editando aluno

--- app/src/main/java/alura/com/br/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/alura/com/br/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,5 +1,6 @@
 package alura.com.br.agenda.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,13 +25,14 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
-
         setTitle(TITULO_APPBAR);
-
-
         inicializacaoDosCamps();
-
         configuraBotaoSalvar();
+        Intent dados = getIntent();
+        Aluno aluno = (Aluno) dados.getSerializableExtra("aluno");
+        campoNome.setText(aluno.getNome());
+        campoTelefone.setText(aluno.getTelefone());
+        campoEmail.setText(aluno.getEmail());
     }
 
     private void configuraBotaoSalvar() {
--- app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/alura/com/br/agenda/ui/activity/ListaAlunosActivity.java
@@ -2,16 +2,23 @@ package alura.com.br.agenda.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 import alura.com.br.agenda.DAO.AlunoDAO;
 import alura.com.br.agenda.R;
+import alura.com.br.agenda.model.Aluno;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -22,10 +29,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
-
         setTitle(TITULO_APPBAR);
-
         configuraFabNovoAluno();
+        dao.salva(new Aluno("Halex Penha Ramos", "(16) 99999-9999", "halex@gmail.com"));
+        dao.salva(new Aluno("Igor Penha Ramos", "(16) 99999-9999", "igor@gmail.com"));
     }
 
     private void configuraFabNovoAluno() {
@@ -41,6 +48,16 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     private void configuraLista() {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
+        final List<Aluno> alunos = dao.todos();
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Aluno alunoEscolhido = alunos.get(i);
+                Intent vaiParaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class);
+                vaiParaFormularioActivity.putExtra("aluno", alunoEscolhido);
+                startActivity(vaiParaFormularioActivity);
+            }
+        });
     }
 }
